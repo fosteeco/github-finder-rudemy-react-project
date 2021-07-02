e6798df5c8c5c754be212c29a05b6f535242c746
Christian Foster <fosterchris000@gmail.com>
2021-07-01 22:55:59 -0400

    Functionality complete, onto impleneting function components and hooks.

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -2,12 +2,14 @@ import React, { Component, Fragment } from "react";
 import Spinner from "../layout/Spinner";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import Repos from "../repos/Repos";
 
 export class User extends Component {
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
     getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
     getUserRepos: PropTypes.func.isRequired,
   };
   componentDidMount() {
@@ -31,7 +33,7 @@ export class User extends Component {
       public_gists,
       hireable,
     } = this.props.user;
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
     if (loading) return <Spinner />;
     return (
       <Fragment>
@@ -99,6 +101,7 @@ export class User extends Component {
           <div className="badge badge-light">Public Repos: {public_repos}</div>
           <div className="badge badge-dark">Public Gists: {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
